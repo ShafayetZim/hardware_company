03bde50c7f6b402e45b40551f844777b1ab064fb
ShafayetZim <shafayet.zim@northsouth.edu>
2023-06-18 10:56:15 +0600

    partners page

---
+++ partners.php
@@ -0,0 +1,171 @@
+<?php
+	include 'header.php';
+?>
+<!-- end of header -->
+
+<div class="partners_sections">
+    <div class="container-fluid">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <h1 class="text-center partners_heading">Our Overseas Partners</h1>
+                    <div class="content-title-underline"></div>
+                </div>
+                  <div class="col-md-12">
+                    <div class="partners_block_item">                      
+                       <div class="partners_content_block" >
+                            <div class="img_sec">
+                                <img class="partners_img rounded" src="images/doble.png">
+                            </div>
+                            <div class="partners_content clearfix"> 
+                                <strong> Doble Engineering Company, USA.</strong> <br>
+                                •   Complete diagnostic solutions of power equipments.<br>
+                                •   Complete electrical asset analysis and condition monitoring.<br>
+                                •   Complete range of electrical test equipment, measuring instruments, predictive, periodic and preventive maintenance and products. <br>
+                               <a href="http://www.doble.com" target="_blank">www.doble.com</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-md-12">
+                    <div class="partners_block_item">
+                        <div class="partners_content_block">
+                            <div class="img_sec">
+                                <img class="partners_img rounded img-responsive" src="images/alt2.PNG"> 
+                            </div>                    
+                            <div class="partners_content clearfix" >                            
+                                <strong>Altanova Group, Italy.</strong> <br>
+                                <li>Online and offline condition monitoring of transformers GIS and cables.</li>
+                                <li>Complete range of electrical test equipment, measuring instruments, predictive, periodic and preventive maintenance and products.</li>
+                                <a href="https://www.altanova-group.com/en" target="_blank">www.altanova-group.com</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+
+                <div class="col-md-12">
+                    <div class="partners_block_item">                      
+                       <div class="partners_content_block" >
+                           <div class="img_sec">
+                                <img class="partners_img rounded" src="images/phenix.png">
+                            </div>
+                            <div class="partners_content">
+                                <strong> Phenix Technologies Inc., USA. </strong> <br>
+                                <b>Phenix Systems AG, Switzerland.</b> <br>
+                                ( A Doble Company )<br>
+                                <li>High Voltage, High Current, High Power testing equipments and components.</li>
+                                <li>Modern HV, EHV and UHV electrical laboratory.</li>
+                                <a href="http://www.phenixtech.com" target="_blank">www.phenixtech.com</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                
+                <div class="col-md-12">
+                    <div class="partners_block_item">
+                        <div class="partners_content_block">
+                            <div class="img_sec">
+                                <img class="partners_img rounded img-responsive" src="images/morgan.png"> 
+                            </div>                    
+                            <div class="partners_content clearfix" >                            
+                                <strong>Morgan Schaffer, Canada.</strong> <br>
+                                ( A Doble Company )<br>
+                                <li>Accuracy by design</li>
+                                <li>Pioneer in most reliable and accurate lab package, field package, online DGA monitoring system.</li>
+                                <a href="http://www.doble.com/about/morgan-schaffer" target="_blank">www.doble.com/about/morgan-schaffer</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-md-12">
+                    <div class="partners_block_item">
+                        <div class="partners_content_block">
+                            <div class="img_sec">
+                                <img class="partners_img rounded img-responsive" src="images/apsensing.png"> 
+                            </div>                    
+                            <div class="partners_content clearfix" >                            
+                                <strong>AP SENSING, Germany</strong> <br>
+                                <li>Most reliable Sub-sea and underground HV power cable condition monitoring solutions.</li>
+                                <li>Technologies used:
+                                
+                                  <dt>&nbsp&nbsp&nbsp- Distributed Temperature Sensing (DTS)  </dt>
+                                  <dt>&nbsp&nbsp&nbsp- Distributed Acoustic Sensing (DAS/DVS)</dt>
+                                  <dt>&nbsp&nbsp&nbsp- Linear Heat Detection (LHD)</dt>
+                                  <dt>&nbsp&nbsp&nbsp- SmartVision</dt>
+                                </li>
+                                
+                                <a href="http://www.apsensing.com" target="_blank">www.apsensing.com</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                
+                <div class="col-md-12">
+                    <div class="partners_block_item">
+                        <div class="partners_content_block">
+                            <div class="img_sec">
+                                <img class="partners_img rounded img-responsive" src="images/infratec.png"> 
+                            </div>                    
+                            <div class="partners_content clearfix" >                            
+                                <strong>INFRATEC GmbH, Germany</strong> <br>
+                                <li>Thermography and sensor technology specialist.</li>
+                                <li>Wide range of Infrared or Thermal Imaging Cameras along with adjustable ranges of powerful lenses.</li>
+                                <a href="http://www.infratec.de" target="_blank">www.infratec.de</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                  
+                <div class="col-md-12">
+                    <div class="partners_block_item">
+                        <div class="partners_content_block">
+                            <div class="img_sec">
+                                <img class="partners_img rounded img-responsive " src="images/mizan.jpeg">
+                            </div>   
+                             <div class="partners_content clearfix">
+                                <strong>Sinohydro Corporation No. 15 Company Limited, P. R. China</strong><br>
+                                •   Roads & High Ways<br>
+                                •   Bridges<br>
+                                •   Railways <br>
+                                •   Metro Rails <br>
+                                •   Aviation<br>
+                                •   Stadium</br><br>
+                                <a href="http://www.powerchina.cn/"
+                                target="_blank">www.powerchina.cn</a>
+                            </div>
+                        </div>    
+                    </div>
+                </div>
+                <div class="col-md-12">
+                    <div class="partners_block_item">
+                        <div class="partners_content_block">
+                            <div class="img_sec">
+                                <img class="partners_img rounded img-responsive " src="images/powerchina.png">
+                            </div>   
+                             <div class="partners_content clearfix">
+                                <strong>Power China International Group Limited, P. R. China
+</strong><br>
+                                •   Power Plants<br>
+                                •   HV & EHV Transmission Lines<br>
+                                •   HV & EHV Sub Stations<br>
+                                •   Solar Projects <br>
+                                •   EPC in Multidisciplinary Sectors <br>
+                                •   Including Hybrid And Nuclear <br>
+                                 <a href="http://www.powerchina-intl.com" target="_blank">www.powerchina-intl.com</a>
+                            </div>
+                        </div>    
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+<!-- footer area -->
+<?php
+	include 'footer.php';
+?>
