13b8deca3b37d66b8d2a403cb9bb5b26432841d6
ShafayetZim <shafayet.zim@northsouth.edu>
2023-06-18 11:02:26 +0600

    about page

---
+++ about.php
@@ -0,0 +1,439 @@
+<?php
+		include 'header.php';
+?>
+<div class="about_section">
+	<div class="container-fluid">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-12 col-sm-12 col-lg-12">
+					<div class="section_heading">
+						<h2 class="text-center">HISTORY PROFILE</h2>
+						<div class="content-title-underline"></div>
+					</div>	
+				</div>
+
+				<div class="col-md-12 col-sm-12 col-lg-12">
+					<div class="about_content">
+					<p>
+<strong>M2K Technology & Trading Co.</strong>, Bangladesh was formed in the year of 2000 by Engineer Habibur Rahman. 
+Initially the company started business in the field of Electrical Power Generation, Power Transmission and Power Distribution. 
+Major customers of the company <strong>M2K Technology & Trading Co.</strong>, are Public Power Utilities and private power utilities
+within Bangladesh. <strong>Bangladesh Standards & Testing Institute (BSTI)</strong>, Bangladesh University of Engineering & Technology (BUET),
+<strong>Bangladesh Atomic Energy Commission (BAEC)</strong>, Bangladesh Council of Scientific Industrial Research (BCSIR), 
+<strong>Bangladesh Institute of Technologies (BITs)</strong> <strong>Directorate of Technical Education (DTE), </strong>
+University of Textile Engineering, Bangladesh (UTE) Abul Khair Group, PHP Group, Bashundhara Group, BSRM, MARICO, Energypac Engineering Ltd,
+Manufacturers, EPC companies and other Government agencies, Semi-Government agencies, Autonomous Statutory Boards and Private customers within the territory of Bangladesh.  As a continuation M2k has entered into EPC infrastructure Projects along with it’s chineese partner Sinohydro Corporation and EPC Power Projects along with it’s another giant chineese partner Power China.
+</p>
+				   </div>    <!-- end of about_content	 -->
+				</div>
+
+				<div class="col-md-12 col-sm-12 col-lg-12">
+					<div class="section_heading">
+						<h2 class=" text-center">GENERAL INFORMATION</h2>
+						<div class="content-title-underline"></div>
+					</div>
+				</div>
+
+				<div class="col-md-12 col-sm-12 col-lg-12">
+					<div class="general_information_sec">
+						<table class="table table-bordered">
+						  <tbody>
+						    <tr>
+						      
+						      <td> <b>Name of the Firm</b> </td>
+						      <td>M2K TECHNOLOGY & TRADING CO.</td>
+						     
+						    </tr>
+						    <tr>
+						      
+						      <td> <b>Head Office Address</b> </td>
+						      <td>Amin Court Building (2nd floor), 62-63 Motijheel Commercial Area, Dhaka – 1000, Bangladesh</td>
+						    
+						    </tr>
+						    <!--<tr>
+						      <td>Overseas office</td>
+						      <td>M2K TECHNOLOGY & TRADING CO. PTE. LTD, Block 428 Clementi Avenue 3 # 10-430 Singapore 120428
+							  </td>
+						    </tr>-->
+
+						    <tr>
+						      <td> <b>Corporate Status</b> </td>
+						      <td> Private Ownership Company </td>
+						    </tr>
+
+						    <tr>
+						      <td> <b>Trade License No.</b> </td>
+						      <td> 028193 </td>
+						    </tr>
+
+						    <tr>
+						      <td> <b>VAT Registration No.</b>  </td>
+						      <td> 9121045253/90402 </td>
+						    </tr>
+						   
+						    <tr>
+						      <td> <b>Taxpayers Identification No.</b>  </td>
+						      <td> 534279263993 </td>
+						    </tr>
+
+						    <tr>
+						      <td> <b>Telephone</b>  </td>
+						      <td> +880-2-5716-0955 </td>
+						    </tr>
+
+						    <!--<tr>
+						      <td> Telephone (Singapore)  </td>
+						      <td> +65-8299-8715 </td>
+						    </tr>-->
+
+						    <tr>
+						      <td> <b>Fax</b>    </td>
+						      <td> +880-2-5716-0966 </td>
+						    </tr>
+
+						    <tr>
+						      <td> <b>E-mail</b>    </td>
+						      <td> info@m2kttc.com </td>
+						    </tr>
+						    
+						    <tr>
+						      <td> <b>Website</b>    </td>
+						      <td> www.m2kttc.com </td>
+						    </tr>
+
+						    <tr>
+						        <th rowspan="4">Bankers</th>
+						        <td>
+
+						                <strong>Bank Asia Limited </strong> <br>
+			                                       MCB Banani Branch <br>
+			                                       42, Kemal Ataturk Avenue, Banani, Dhaka 1213<br>
+			                            <strong>Account No:</strong> 01233054565 <br>
+			                            <strong>Swift : </strong> BALBBDDH012 <br>
+                                </td>
+						        
+						    </tr>
+						  
+						    <tr>
+						        
+						    	<td>
+
+						                <strong>Premier Bank Ltd.</strong> <br>
+			                                        Banani Branch <br>
+			                                        Iqbal Center (2nd floor)  <br>
+			                                        24, Kemal Ataturk Avenue, Dhaka 1213, Bangladesh  <br>
+			                            <strong>Account No:</strong> 010411100082668 <br>
+			                            <strong>Swift : </strong> PRMRBDDHBNN<br>
+                                  </td>
+						        
+						    </tr>
+
+						    <tr>
+
+
+                                <td>
+
+						               <strong>The City Bank Ltd.</strong>  <br>
+			                                        Foreign Exchange Branch <br>
+			                                        27, Baitul Hossain Building (Ground floor) <br>
+			                                        Dilkusha Commercial Area, Dhaka-1000, Bangladesh <br>
+			                           <strong>Account No:</strong> 1502470689001 <br>
+			                           <strong>Swift : </strong> CIBLBDDH232 <br>
+                                    			  </td>
+
+
+
+						    </tr>
+
+						    <tr>
+
+						    	<td>
+
+						                <strong>Agrani Bank Ltd.</strong> <br>
+			                                        Foreign Exchange Corporate Branch <br>
+			                                        1/B, Rajuk (DIT) Avenue, Motijheel Commercial Area, Dhaka-1000, Bangladesh.  <br>
+			                            <strong>Account No:</strong> 0200012453376 <br>
+			                            <strong>Swift : </strong> AGBKBDDH006 <br>
+                                  </td>
+
+						    </tr>
+
+						    
+
+
+
+
+						    
+						    <!--<tr>
+						    	<td>
+						            
+						                <strong>OCBC Bank</strong> <br>
+			                                        Blk 637 Bukit Batok Central #01-16, Singapore 650637<br>
+			                                        Account No: <br> 
+			                                          <strong>503476954301 (USD) </strong> <br>
+			                                        <strong>  713029072001 (SGD) </strong>	
+                                    			 </td>
+						        
+						    </tr>-->
+
+		                    </tbody>
+						</table>
+				   </div>    
+				   <!-- general information section	 -->
+
+			     </div>  	<!--col-md-12  -->
+			     
+			     <!-- customer reference -->
+			     <div class="col-md-12 col-sm-12 col-lg-12">
+					<div class="section_heading">
+						<h2 class=" text-center">CUSTOMER REFERENCES</h2>
+						<div class="content-title-underline"></div>
+					</div>
+				</div>
+
+
+				 <div class="col-md-12 col-sm-12 col-lg-12">
+					<div class="customer_refernce_sec">
+						<ol>
+							<li>Bangladesh Power Development Board (BPDB)</li>
+							<li>Power Grid Company of Bangladesh Ltd. (PGCB)</li>
+							<li>Bangladesh Rural Electrification Board (BREB)</li>
+							<li>Dhaka Power Distribution Company Ltd. (DPDC)</li>
+							<li>Dhaka Electric Supply Company Ltd. (DESCO)</li>
+							<li>Ashuganj Power Station Company Limited (APSCL)</li>
+							<li>West Zone Power Distribution Company Limited (WZPDCL)</li>
+							<li>North West Power Generation Company Ltd. (NWPGCL)</li>
+							<li>Rural Power Company Ltd. (RPCL)</li>
+							<li>Electricity Generation Company of Bangladesh (EGCB)</li>
+							<li>Northern Electricity Supply Company Ltd. (NESCO)</li>
+							<li>Coal Power Generation Company of Bangladesh Ltd. (CPGCBL)</li>
+	                        <li>Roads and Highways Division</li>
+	                        <li>Local Government & Rural Development</li>
+	                        <li>Bangladesh Bridge Authority</li>
+	                        <li>Local Government & Engineering Development</li>
+							<li>Public Works Division (PWD)</li>
+							<li>Civil Aviation Authority of Bangladesh (CAAB)</li>
+							<li>Military Engineering Services (MES) </li>
+							<li>Summit Power Generation Co.</li>
+							<li>Powerpac Power Generation Co.</li>
+							<li>SINHA Power</li>
+							<li>Energypac Power Generation Company Ltd.</li>
+							<li>Confidence Power Ltd.</li>
+							<li>United Power Generation & Distribution Company Ltd. (UPGDCL)</li>
+							<li>Directorate General of Defense Purchase (DGDP) </li>
+							<li>Bangladesh University of Engineering & Technology (BUET)</li>
+							<li>Bangladesh Standard Testing Institution (BSTI)</li>
+							<li>Bangladesh Atomic Energy Commission (BAEC)</li>
+							<li>Bangladesh Council of Scientific Industrial Research (BCSIR)</li>
+							<li>Bangladesh Institute of Technologies (BITs)</li>
+							<li>Directorate of Technical Education (DTE)</li>
+							<li>University of Textile Engineering, Bangladesh (UTE)</li>
+
+							<li>Energypac Engineering Ltd.</li>
+							<li>System Engineering Ltd.</li>
+							
+							<li>Reverie Power & Automation Engineering Ltd.</li>
+							<li>Polaris Power Engineering Ltd.</li>
+							<li>BBS Cables Limited</li>
+							<li>Adex Engineering Ltd.</li>
+							<li>Energypac Cable Ltd.</li>
+							<li>Powermann Bangladesh Ltd.</li>
+							<li>System Engineering Ltd.</li>
+							<li>Basic Power Engineering Ltd.</li>
+							<li>Lafarge-Holcim Ltd.</li>
+							<li>Abul Khair Group, Bangladesh</li>
+							<li>PHP Group, Bangladesh</li>
+							<li>Bashundhara Group, Bangladesh</li>
+							<li>BSRM, Bangladesh</li>
+							<li>EPC and Erection Companies</li>
+							<li>And Many Others.</li>
+
+
+						</ol>
+					</div>	
+				 </div>
+				</div>
+			     <!-- annual section -->
+			     <div class="col-md-12 col-sm-12 col-lg-12">
+			     	<div class="section_heading">
+			     	  <h3 class="text-center">ANNUAL TURNOVER WITHIN LAST 5 YEARS</h3>
+			     	  <div class="content-title-underline"></div>	
+			     	</div>
+			     </div>
+
+			     <!-- annual section close -->
+			     <div class="col-md-12 col-sm-12 col-lg-12">
+			     	<div class="annual_content">
+			     		 <table class="table table-bordered text-center">
+						    <thead>
+						      <tr>
+						        <th>FISCAL YEAR</th>
+						        <th>ANNUAL TURN OVER</th>
+						      </tr>
+						    </thead>
+						    <tbody>
+						      
+						      <tr>
+						        <td>2018 - 2019</td>
+						        <td>US$ 13.06 Million</td>
+						        
+						      </tr>
+						      <tr>
+						        <td>2019 - 2020</td>
+						        <td>US$ 15.97 Million</td>
+						        
+						      </tr>
+						      </tr>
+						      <tr>
+						        <td>2020 - 2021</td>
+						        <td>US$ 18.72 Million</td>
+						        
+						      </tr>
+						      </tr>
+						      <tr>
+						        <td>2021 - 2022</td>
+						        <td>US$ 23.39 Million</td>
+						        
+						      </tr>
+						      </tr>
+						      <tr>
+						        <td>2022 - 2023</td>
+						        <td>US$ 31.89 Million</td>
+						        
+						      </tr>
+						    </tbody>
+						  </table>
+			     	</div>   <!-- annual section -->
+			     </div>   <!-- col-md-12   -->
+
+			     <div class="col-md-12 col-lg-12 col-sm-12">
+			     	<div class="section_heading">
+			     		<h3 class="text-center"> ADVISORS AND CONSULTANTS</h3>
+			     		 <div class="content-title-underline"></div>	
+			     	</div>
+			     </div>
+
+
+				<!-- advisor content section -->
+			     <div class="col-md-12 col-lg-12 col-sm-12">
+			     	<div class="advisors_content">
+			     		<ol>
+			     			<li>  <strong>Mr. Feroz Ahmed </strong> <br>
+									Managing Director <br>
+									Creative Paper Mills Ltd.
+							</li>
+							<li> <strong> Engineer Anwarul Karim </strong> <br> 
+								<p>Graduated in Electrical & Electronics Engineering from Bangladesh University of Engineering & Technology (BUET) in 1975. He joined Dhaka Electric Supply Authority (DESA) in 1976. Mr. Karim left the country in 1978 and took up a job under the Ministry of Electricity, Govt. of Libya and was posted at various Power Stations and Distribution Authorities, He returned to Bangladesh in 1996 and joined M2K Technology &Trading Co. as Senior Consultant of the company in the year of 2002. </p>
+
+								<p>Mr. Karim has extensive knowledge in LV, MV and HV area. He worked for various private power companies, for example, United Group, Summit Group, Bashundhara Group, CPML, Biswas Builders and many other similar public and private power utilities both in Bangladesh and abroad.</p>	
+
+							</li>
+
+							<li>
+								 <strong>Engineer Tanveer Ehsanur Rahman</strong>  <br>
+								 <p>
+								 Graduated in Civil Engineering from Bangladesh University of Engineering & Technology (BUET). Mr. Tanveer Ehsanur Rahman is a key Director of Novo Group and Spectrum Engineers & Software Ltd.
+								</p>
+
+							</li>
+
+							<li>  <strong>Engineer Probir Gopal Roy</strong>
+								<p>Graduated in Civil Engineering from Bangladesh University of Engineering & Technology (BUET). Mr. Roy is responsible for civil design, Drawing and construction of the company.</p>
+							 </li>
+
+							 <li> <strong>Engineer Zakir Mahmud </strong> 
+							   <p>
+							 	Graduated in Electrical & Electronics Engineering from Bangladesh University of Engineering & Technology (BUET). Mr. Mahmud is working as Divisional Engineer in Public Telecom Company Bangladesh Telecommunications Company Limited (BTCL).
+							 	</p> 
+							 </li>
+
+							 <li> <strong>Engineer Zaheer Uddin Md. Babar </strong>  
+							 	<p>Graduated in Mechanical Engineering from (BUET). Mr. Babar is working as CEO of Baximco and also is responsible for Mechanical Drawing Design and works of the Company.  </p>
+							 </li>
+							 							 <li> <strong>Engineer Zakirul Kabir Khan </strong>  
+							 	<p>Graduated in Electrical & Electronics Engineering from Khulna University of Engineering & Technology (KUET). Mr. Zakirul Kabir Khan is working as executive  Engineer in Public Power utility Bangladesh Power Development Board (BPDB).</p>
+							 </li>
+
+							 <div class="col-md-12">
+                    
+                </div>
+						 </ol>
+			     		</div>
+			     </div>
+
+			     <!-- advisor content closed -->
+
+			     <div class="col-sm-12 col-md-12 col-lg-12">
+			     	<div class="section_heading">
+			     		 <h3 class="text-center ">LEGAL ADVISOR  </h3>
+			     		 <div class="content-title-underline"></div>
+
+			     		 <div class="legal_advisor_content">
+			     		 	<strong>Sheikh & Chowdhury </strong> <br>
+								Banglar Bani Bhaban (2nd floor) <br>
+								81 Motijheel Commercial Area <br>
+								Dhaka-1000, Bangladesh <br>
+								<strong>Tel:</strong> +88-02-9557548 <br>
+								<strong>Fax:</strong> +88-02-9562885 <br>
+								<strong>E-mail:</strong> sheikhnchowdhury@gmail.com
+			     		 </div>
+			     	</div>
+			   </div>
+
+			   <div class="col-sm-12 col-md-12 col-lg-12">
+			     	<div class="section_heading">
+			     		 <h3 class="text-center ">ASSOCIATE COMPANIES </h3>
+			     		 <div class="content-title-underline"></div>
+
+			     		 <div class="friends_companies">
+			     		    <ol>
+			     		        <li> M2K Technologies</li>
+			     		        <li> HeptaTech Developments Limited</li>
+			     		        <li> HeptaTech Limited</li>
+			     		        <li> Creative Engineers Limited</li>
+			     		        <li> Creative Paper Mills Limited</li>
+			     		        <li> Pace International</li>
+			     		        <li> Taxila Impex PTE Limited, Singapore</li>
+                                <li> CEL Trillium Inc., Canada</li>
+                                <li> BNX Networks Inc., USA</li>
+
+                                <!--
+                                <li> Desh Engineering.</li>
+                                <li> ERA Construction., Bangladesh</li>
+                                <li> MRM International., Bangladesh</li>
+                                <li> Techsol Engineering Ltd., Bangladesh</li>
+                                -->
+
+
+    							
+    							
+    							
+    							
+    							
+    							
+							</ol> 
+			     		 	
+			     		 </div>
+			     	</div>
+			   </div>
+
+
+			   
+
+
+
+
+
+
+			</div>			
+		</div>
+	</div>
+</div>
+
+
+
+
+
+<?php
+	include 'footer.php';
+?>
