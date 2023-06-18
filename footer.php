927cb8c4715e97e2b33bee89d9cf76b6e01f7f79
ShafayetZim <shafayet.zim@northsouth.edu>
2023-06-18 10:50:23 +0600

    footer file

---
+++ footer.php
@@ -0,0 +1,73 @@
+<footer class="footer">
+        <div class="container-fluid"> 
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-6">
+                        <div class="location-section">
+                            <h3>Office Details :</h3>
+                            <p>  
+                                M2K TECHNOLOGY & TRADING CO., BANGLADESH<br>
+                                Amin Court Building (2nd floor), 62-63 Motijheel<br>
+                                Commercial Area, Dhaka-1000, Bangladesh<br><br>
+                                Telephone: +880-2-5716-0955<br>
+                                Fax: +880-2-5716-0966<br>
+                                Hotline: +880-170-780-9997<br>
+                                E-mail: info@m2kttc.com
+                            </p>     
+                        </div>
+                    </div>
+                    <!--<div class="col-md-6">
+                        <div class="location-section singapore">
+                            <h3>Singapore Office</h3>
+                            <p>
+                                M2K Technology & Trading Co. Pte. Ltd. <br>
+                                Block 428 Clementi Avenue 3 # 10-430<br>
+                                Singapore 120428 <br><br>
+                                Telephone: +65-8299-8715<br>
+                                E-mail: info@m2kttc.com <br>
+                            </p>
+                        </div>
+                    </div>-->
+                  
+                </div>
+                <div class="row">
+                    <div class="col-md-12 quick-link">
+                        <h3>Quick Links:</h3>
+                        <ul>
+                            <li><a href="">About</a></li>
+                            <li><a href="">Videos</a></li>
+                            <li><a href="">Operating Menuals</a></li>
+                            <li><a href="">Career</a></li>
+                            <li><a href="">Trade Show</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-12 social-link">
+    					<ul>
+                            <li>
+                                <a href=""><i class="fab fa-facebook-square" aria-hidden="true"></i></a>
+                            </li>
+                            <li>
+                                <a href=""><i class="fab fa-twitter-square" aria-hidden="true"></i></a>
+                            </li>                            
+                            <li>
+                                <a href=""><i class="fab fa-linkedin" aria-hidden="true"></i></a>
+                            </li>
+                        </ul>                					
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-12 copyright">
+                        <h5>Copyrights @2023 M2K Technologies.All right reserved.</h5>
+                    </div>
+                </div>
+            </div>
+        </div>         
+    </footer>
+    
+    <script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="crossorigin="anonymous"></script>
+    <script src="js/bootstrap.min.js"></script>
+	
+</body>
+</html>
