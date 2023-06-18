3359d9976de4220f592d234903ac05050ab1c16e
ShafayetZim <shafayet.zim@northsouth.edu>
2023-06-18 10:43:41 +0600

    home page

---
+++ index.php
@@ -0,0 +1,146 @@
+<?php
+		include 'header.php';
+?>
+
+    <!-- slider -->
+    <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+                <img class="d-block w-100" src="images/products/powerplant.jpg" alt="First slide">
+            </div>
+            
+            <div class="carousel-item">
+                <img class="d-block w-100" src="images/slide4.jpg" alt="First slide">
+            </div>
+            
+            <div class="carousel-item">
+                <img class="d-block w-100" src="images/slide02.png" alt="Second slide">
+            </div>
+            <div class="carousel-item">
+                <img class="d-block w-100" src="images/slide03.png" alt="Third slide">
+            </div>
+        </div>
+        <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+        </a>
+        <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+        </a>
+    </div>
+
+    <div class="container-fluid">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6 middle-section">
+                    <div class="advantage-section">
+                        <h2>The M2K Advantages</h2>
+
+ <ul type="square">                      
+ <li>Specific, Measurable, Accurate and Trustworthy.</li>  
+ <li>A complete line of Testing and Measuring systems, EPC Power and Infrastructure projects .</li>
+ <li>Online and offline electrical asset analysis and condition monitoring solutions and services.  </li>
+ <li>All Electricals under one umbrella from single source.  </li>
+ <li>Superior quality, reliability and versatility for decades of extended service life. </li>
+ <li>Innovative technical and technological solutions.</li>
+ <li>Prompt response (24 Hours hotline). </li>
+ <li>Optimum safety features.</li>
+ <li>Operating software that provides automation, data acquisition, remote control and capabilities to
+ meet demanding testing applications.  </li>
+ <li>Over 24+  years' experience.  </li>
+</ul>
+
+
+
+
+
+
+
+                    </div>
+                </div>
+                <div class="underline-vertical"></div>
+                <div class="col-md-5 middle-section">
+                    <div class="prodect-section">
+                        <div class="product-header">
+                            <h2>Representative of</h2>
+                        </div>
+                        <div class="resident">  
+                        <p>1. Doble Engineering Company, USA</p>
+                            <p>
+                                <a href="https://www.doble.com" target="_blank">www.doble.com</a>
+                            </p>
+                            
+                            <p>2.  Altanova Group, Italy  </p>
+                            <p><a href="https://altanova-group.com" target="_blank">www.altanova-group.com</a></p>
+                            
+                            <p>3. Phenix Technologies Inc., USA</p>
+                            <p>
+                                <a href="http://www.phenixtech.com" target="_blank">www.phenixtech.com</a>
+                            </p>
+                            
+                            
+                            
+                            
+
+                            <p>4. Morgan Schaffer, Canada</p>
+                            <p>
+                                <a href="http://www.doble.com/about/morgan-schaffer" target="_blank">www.doble.com/about/morgan-schaffer</a>
+                            </p>
+                            
+                            
+                             <p>5. AP Sensing, Germany  </p>
+                            <p>
+                                <a href="https://apsensing.com" target="_blank">www.apsensing.com</a>
+                            </p>
+                            
+                            <p>6. Infratec, Germany  </p>
+                            <p>
+                                <a href="https://www.infratec.eu/" target="_blank">www.infratec.eu</a>
+                            </p>
+                       
+                          
+                            
+                           
+                            
+                            <p>7. Sinohydro Corporation No. 15 Company Limited, P. R. China  </p>
+                            <p>
+                                <a href="http://www.powerchina.cn" target="_blank">www.powerchina.cn</a>
+                            </p>
+                            
+                            <p>8. Power China International Group Ltd., P. R. China  </p>
+                            <p>
+                                <a href="http://www.powerchina-intl.com/" target="_blank">www.powerchina-intl.com</a>
+                            </p>
+                            
+                        
+                            
+        
+                        					
+							
+                        </div>               
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="subscribe-section">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6 subscribe-content">
+                    <h2>Subscribe to Our Newsletter</h2>
+                </div>
+                <div class="col-md-3 subscribe-content">
+                    <input type="text" class="form-control" placeholder="Email">
+                </div>
+                <div class="col-md-3 subscribe-content">
+                    <a href="#" class="btn btn-outline-primary btn-lg">Subscribe</a>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <?php
+		include 'footer.php';
+	?>
