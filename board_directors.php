6d1e7ba9a34e188ca10090161dfec057fce46ec3
ShafayetZim <shafayet.zim@northsouth.edu>
2023-06-18 11:04:17 +0600

    bod page

---
+++ board_directors.php
@@ -0,0 +1,86 @@
+<?php
+	include 'header.php';
+?>
+	<!-- end of header  -->
+
+
+<div class="board_of_director">
+	<div class="container-fluid">
+		<div class="container">
+			<div class="row">
+
+				<div class="col-md-12 col-sm-12 col-lg-12">
+					<div class="member_item_block">
+						<img class="img-resposive text-center" src="images/BOD/director4.jpg">
+						<p class="name"style="margin-top:-10px"> Md. Habibur Rahman</p>
+						<p class="position"style="margin-top:-17px">Chairman, Managing Director & CEO</p>
+						<p class="position"style="margin-top:-20px">B.Sc. in EEE (BUET)
+					</div>
+				</div>
+				
+				<div class="col-md-12 col-sm-12 col-lg-12">
+					<div class="member_item_block">
+						<img class="img-resposive text-center" src="images/BOD/manadirector-1.jpg">
+						<p class="name"style="margin-top:-10px"> Mahfuzur Rahman </p>
+						<p class="position"style="margin-top:-17px">Director</p>
+						<p class="position"style="margin-top:-20px">B.Sc. in EEE (BUET)</p>
+					</div>
+				</div>
+
+
+				
+				
+				
+					
+					<!--<div class="col-md-4 col-sm-4 col-lg-4">
+
+					<div class="member_item_block">
+						<img class="img-resposive text-center" src="images/BOD/director.jpg">
+						<p class="name">Dr. Kazi Umme Kulsum 	
+						</p>
+						<p class="position">Director</p>
+					</div>
+					
+				</div>-->
+
+
+               	<!-- <div class="col-md-4 col-sm-4 col-lg-4">
+					<div class="member_item_block">
+						<img class="img-resposive text-center" src="images/BOD/blank_male.jpg">
+						<p class="name">Engineer Arif Mahmood</p>
+						<p class="position">Director</p>
+					</div>
+                </div>-->
+                
+				<center></center><div class="col-md-4 col-sm-4 col-lg-12">
+					<div class="member_item_block">
+						
+					
+				
+					</div>
+				</div></center>
+
+				
+
+		<!--<div class="col-md-12 col-sm-12 col-lg-12">
+					<div class="member_item_block">
+						<img class="img-resposive text-center" src="images/BOD/director4.jpg">
+						<p class="name">Engineer Habibur Rahman </p>
+						<p class="position">Managing Director</p>
+					</div>
+				</div>-->
+
+				
+				
+				
+			</div>
+		</div>
+	</div>
+</div>
+
+
+
+
+<?php
+	include 'footer.php';
+?>
