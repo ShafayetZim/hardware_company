56b37f73a142f11a492836c4d15a2ee083933648
ShafayetZim <shafayet.zim@northsouth.edu>
2023-06-18 10:48:58 +0600

    header file

---
+++ header.php
@@ -0,0 +1,97 @@
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Millenium 2000</title>
+	<link rel="icon" type="image/ico" href="images/logo.jpg" />
+    <link rel="stylesheet" href="css/bootstrap.min.css">    
+    <!-- <link rel="stylesheet" href="font-awesome/font-awesome.min.css"> -->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css">
+	<link href="css/all.min.css" rel="stylesheet" type="text/css">
+    <link rel="stylesheet" type="text/css" href="css/animate.css">
+    <link href="css/jquery.fancybox.css" rel="stylesheet" />
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <link rel="stylesheet" type="text/css" href="css/responsive.css">
+</head>
+<body>
+    <header>
+        <div class="container-fluid">
+            <div class="container">
+                <div class="row">                   
+                    <div class="col-md-9 logo-area">
+                        <div class="logo">
+                            <a href="index.php">
+                                <img class="img-responsive" src="images/logo.jpg" />
+                            </a> 
+                        </div>
+                        <div class="mk-text">
+							
+                            <p> <span class="since_text">Since 2000 ...</span> <br>
+    							M2K TECHNOLOGY & TRADING CO., Bangladesh<br>
+                                <!--M2K Technology & Trading Co. Pte. Ltd., Singapore-->
+                            </p>
+                        </div>
+                    </div>                        
+                    <div class="col-md-3 mk-call">
+                        <p><span class="telephone"><b>Tel</b> :</span>  +880-2-5716-0955</p>
+                        <p><span class="email telephone"><b>E-mail</b> :</span>  info@m2kttc.com</p>
+                    </div>                    
+                </div>
+            </div>            
+        </div>
+        <div class="underline"></div>
+    </header>
+
+    <!-- menu -->
+    <div class="container-fluid">
+        <div class="container">
+            <nav class="navbar navbar-expand-lg navbar-light bg-light">                    
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav mr-auto">
+                        <li class="nav-item">
+                            <a class="nav-link" href="about.php">About Us</a>
+                        </li>
+                        
+                        <li class="nav-item">
+                            <a class="nav-link" href="partners.php">Partners</a>
+                        </li>
+                        <li class="nav-item active">
+                            <a class="nav-link" href="products.php">Products</a>
+                        </li>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link" href="services.php">Services</a>   
+                        </li>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            Download
+                            </a>
+                            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                <a class="dropdown-item" href="https://www.doble.com" target="_blank">Doble</a>
+                                <a class="dropdown-item" href="https://altanova-group.com" target="_blank">Altanova</a>
+                                <a class="dropdown-item" href="http://www.phenixtech.com" target="_blank">Phenix</a>
+                                <a class="dropdown-item" href="http://www.doble.com/about/morgan-schaffer" target="_blank">Morgan Schaffer</a>
+								<a class="dropdown-item" href="https://apsensing.com" target="_blank">AP Sensing</a>
+                                <a class="dropdown-item" href="https://www.infratec.eu/" target="_blank">Infratec</a>
+                                <a class="dropdown-item" href="http://www.powerchina.cn/" target="_blank">Sinohydro</a>
+                                <a class="dropdown-item" href="http://www.powerchina-intl.com/" target="_blank">Power China</a>
+                            </div>
+                        </li>
+						<li class="nav-item">
+                            <a class="nav-link" href="board_directors.php">Directors</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="https://m2kttc.com:2096" target="_blank">Web Mail</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="contact.php">Contact Us</a>
+                        </li>
+                    </ul>                    
+                </div>
+            </nav>    
+        </div>
+    </div>
