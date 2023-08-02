56f866580c15e02ee53f54d6fe8ecbcbaf7b926b
ShafayetZim <shafayet.zim@northsouth.edu>
2023-08-02 18:01:25 +0600

    Event added

--- index.php
+++ index.php
@@ -156,6 +156,20 @@
         </div>
     </div>
 
+    <div class="event-section">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6 event-content">
+                    <h2>Checkout Our Events</h2>
+                </div>
+
+                <div class="col-md-6 subscribe-content">
+                    <a href="event.php" class="btn btn-outline-primary btn-lg">Check</a>
+                </div>
+            </div>
+        </div>
+    </div>
+
 
     <div class="subscribe-section">
         <div class="container">
